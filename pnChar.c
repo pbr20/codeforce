923b0f82376e73c638f196f83679cd4092c91f27
pbr20 <rithen1971@gmail.com>
2024-09-23 20:04:39 +0600

    pk-pt

---
+++ pnChar.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+
+int main(){
+
+char A;
+
+scanf("%c",&A);
+
+if(A>=65 && A<= 90){
+    A = A + 32;
+}
+
+else{
+    A = A -32;
+}
+
+printf("%c",A);
+
+    return 0;
+}
---
+++ ppFirstdigit.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+
+int main(){
+
+int N;
+
+scanf("%d",&N);
+
+N = N/1000;
+
+if(N % 2 == 0){
+    printf("EVEN\n");
+}
+
+else{
+    printf("ODD\n");
+}
+
+    return 0;
+}
